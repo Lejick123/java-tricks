0baa8f84e1e1e1bba45bfefcc35c1eb368c87a31
Lejick <lejick@gmail.com.com>
2019-12-15 22:04:06 +0300

    .

--- src/j8/Main.java
+++ src/j8/Main.java
@@ -59,5 +59,21 @@ public class Main {
 
         optional.ifPresent((s) -> System.out.println(s.charAt(0)));
 
+
+        /**
+         * Нахождение максимального и минимального значений
+         */
+        ArrayList<Integer> testValues = new ArrayList();
+        testValues.add(0,15);
+        testValues.add(1,1);
+        testValues.add(2,2);
+        testValues.add(3,100);
+        testValues.add(4,50);
+
+        Optional<Integer> maxValue = testValues.stream().max(Integer::compareTo);
+        System.out.println("MaxValue="+maxValue);
+        Optional<Integer> minValue = testValues.stream().min(Integer::compareTo);
+        System.out.println("MinValue="+minValue);
+
     }
 }
